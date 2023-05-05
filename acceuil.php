f17654d4514040a1ee7c4966c02fe4d0d2b6e4ee
wallerandL <wallerandlahaussois@gmail.com>
2023-05-05 14:56:15 +0200

    màj à la dernière version

---
+++ acceuil.php
@@ -0,0 +1,17 @@
+<?php  session_start(); 
+$log;
+if (isset ($_SESSION['log']))
+ $log= $_SESSION['log'];   ?>
+
+<!DOCTYPE html>
+<html lang=fr>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    </head>
+    <body>
+    <?php include('entete.php'); ?>
+    <?php 
+    if (!empty($log)) {
+    echo"Bonjour ",$log ;}?>
+    </body>
+</html>
---
+++ entete.php
@@ -0,0 +1,40 @@
+
+<html>
+    <head>
+        <title>acceuil</title>
+        <meta charset='utf-8'>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <style type="text/css">
+        .container {
+            font-family: Verdana, Helvetica, Arial, sans-serif;
+        }
+        
+        .form-control_mr-sm-2{
+            height: 1.8em;
+            width: 32em;
+        }
+    </style>
+    </head>
+    <body>
+        <div class="container">
+                 <div></div>   
+                    <input type="button" onclick=window.location.href='acceuil.php' name="submit" value="Amphore" />
+                    <input type="button" onclick=window.location.href='form_auth.php' name="submit" value="cuisine a bas prix" />
+                    <input type="button" onclick=window.location.href='form_auth.php' name="submit" value="recette aléatoire" />
+                    <input type="button" onclick=window.location.href='form_auth.php' name="submit" value="Recette de saison" />
+                    <input type="button" onclick=window.location.href='form_auth.php' name="submit" value="connexion" />
+                    <input type="button" onclick=window.location.href='deco.php' name="submit" value="deconnexion" />
+
+              <form class="form-inline">
+                <input class="form-control_mr-sm-2" type="search" placeholder="Recherche ce que tu veut tant que ca a un rapport avec la nouriture" aria-label="Search">
+                <button class="btn_btn-outline-info_my-2_my-sm-0" type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </button>
+              </form>
+              <hr class="my-4"> 
+          </div>
+          
+    </body>
+</html>
