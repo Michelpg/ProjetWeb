62ed6c562ceced6934a41b5fd171c6f6b871147e
Michelpg <michel.peng@hotmail.com>
2023-05-05 14:54:39 +0200

    page_accueil

--- projetweb/acceuil.php
+++ projetweb/acceuil.php
@@ -7,6 +7,16 @@
     </head>
     <body>
     <?php include('entete.php'); ?>
+
+	<div class="jumbotron">
+	  <h1 class="display-4">Bienvenue sur Recettes de cuisine !</h1>
+	  <p class="lead">Découvrez des recettes délicieuses et simples à réaliser chez vous.</p>
+	  <hr class="my-4">
+	  <a class="btn btn-primary btn-lg" href="#" role="button">Voir les recettes</a>
+	</div>
+
+
+    
     
     </body>
 </html>
