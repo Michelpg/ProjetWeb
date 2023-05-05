d83f264f665636ccdd0092e3b35c4f5e7922f24a
wallerandL <80842134+wallerandL@users.noreply.github.com>
2023-05-05 08:52:59 +0200

    Add files via upload

--- projetweb/entete.php
+++ projetweb/entete.php
@@ -15,14 +15,15 @@
         }
         .form-control_mr-sm-2{
             background-color: gold;
+            height: 1.8em;
+            width: 32em;
         }
     </style>
     </head>
     <body>
         <div class="container">
-                    <button type="submit" name="submit" value="Marmiton" ><svg aria-hidden="true" viewBox="0 0 10 10">
-    <path d=""/>
-  </svg></button>
+                    
+                    <input type="button" onclick=window.location.href='acceuil.php' name="submit" value="Amphore" />
                     <input type="button" onclick=window.location.href='form_auth.php' name="submit" value="Cours" />
                     <input type="button" onclick=window.location.href='form_auth.php' name="submit" value="cuisine a bas prix" />
                     <input type="button" onclick=window.location.href='form_auth.php' name="submit" value="recette aléatoire" />
@@ -32,12 +33,13 @@
 
               <form class="form-inline">
                 <input class="form-control_mr-sm-2" type="search" placeholder="Recherche ce que tu veut tant que ca a un rapport avec la nouriture" aria-label="Search">
-                <button class="btn btn-outline-info my-2 my-sm-0" type="submit">
+                <button class="btn_btn-outline-info_my-2_my-sm-0" type="submit">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                     </svg>
                 </button>
               </form>
           </div>
+          <hr class="my-4"> 
     </body>
 </html>
