171adde9c3bf7c260a616f835afbd9a2d210aab6
Guillaume BOYER <guillaume.boyer@esme.fr>
2023-05-10 17:13:25 +0200

    ajout de bootstrap css+js

--- acceuil.php
+++ acceuil.php
@@ -7,6 +7,7 @@ if (isset ($_SESSION['log']))
 <html lang=fr>
     <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     </head>
     <body>
     <?php include('entete.php'); ?>
@@ -27,6 +28,6 @@ if (isset ($_SESSION['log']))
     </div>
 
 
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </body>
 </html>
