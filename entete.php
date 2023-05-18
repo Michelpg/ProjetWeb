6f5d60cc78c789d1a375066a4c2b27d3b5e4857d
wallerandL <wallerandlahaussois@gmail.com>
2023-05-18 17:24:27 +0200

    Modification taille image modification recherche pour oublier les anciennes recherche

--- entete.php
+++ entete.php
@@ -10,7 +10,13 @@ if (isset ($_SESSION['log']))
         
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-   
+   <style>
+   .card img {
+  width: 300px; 
+  height: 200px; 
+  object-fit: cover; 
+}
+</style>
     </head>
     <body>
     <div style="height : 100px; display:flex;text-align:right;">
