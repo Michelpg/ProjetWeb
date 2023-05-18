18ddc318f7c9d5a9a58c9186fd9a51365d2c5ea0
Michelpg <michel.peng@hotmail.com>
2023-05-18 17:14:00 +0200

    ajout de la requete sql

--- favoris.php
+++ favoris.php
@@ -6,18 +6,13 @@ $_SESSION['favoris']= $_POST['favoris'];
 $favoris= $_SESSION['favoris'];
 if ($favoris!=''){
     if ($favoris!=' '){
-$select_query = "SELECT * FROM favori;";
+$select_query = "SELECT recette.nom, recette.description, recette.image FROM favori INNER JOIN recette ON favori.id_rec = recette.id_rec;";
 $res = $pdo->query($select_query);
 $res->setFetchMode(PDO::FETCH_ASSOC);
 $recette_bdd = $res->fetch();
+
 $_SESSION['nom_recette'] = $recette_bdd['nom'];
 $_SESSION['description_recette'] = $recette_bdd['description'];
-$_SESSION['ingredient_recette'] = $recette_bdd['ingredient'];
-$_SESSION['difficulte_recette'] = $recette_bdd['difficulte'];
-$_SESSION['cout_recette'] = $recette_bdd['cout'];
-$_SESSION['nbr_pers_recette'] = $recette_bdd['nbr_pers'];
-$_SESSION['ustensiles_recette'] = $recette_bdd['ustensiles'];
-$_SESSION['preparation_recette'] = $recette_bdd['preparation'];
 $_SESSION['image_recette'] = $recette_bdd['image'];
 }else {
     unset($_SESSION['nom_recette']); }
@@ -25,5 +20,5 @@ $_SESSION['image_recette'] = $recette_bdd['image'];
 }else{
     unset($_SESSION['nom_recette']); }
 
-header('location:recherche_recette.php');
+header('location:recherche_favoris.php');
 ?>
